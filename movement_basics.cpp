9043068f4ef9d874966ad4764d31291a27f172ea
Paras Gupta <parasg1999@gmail.com>
2018-09-21 10:40:34 +0530

    Thread Implementation for movement

--- movement_basics.cpp
+++ movement_basics.cpp
@@ -1,49 +1,75 @@
 #include<iostream>
 #include<graphics.h>
 #include<windows.h>
+#include<thread>
+
+using namespace std;
+
+int x=50,y=100;
+
+class Mario {
+public:
+    void moveRight() {
+        while(true) {
+            if(GetAsyncKeyState(VK_RIGHT)) {
+            cout<<"Right";
+            cleardevice();
+            circle(++x,y,50);
+            }
+            delay(3);
+        }
+    }
+    void moveLeft() {
+        while(true) {
+            if(GetAsyncKeyState(VK_LEFT)) {
+            cout<<"Left";
+            cleardevice();
+            circle(--x,y,50);
+            }
+            delay(3);
+        }
+    }
+    void jump() {
+        while(true) {
+            if(GetAsyncKeyState(VK_SPACE)) {
+                for(int i=0; i<50;i++) {
+                    cleardevice();
+                    circle(x,--y,50);
+                    delay(3);
+                }
+                for(int i=0; i<50;i++) {
+                    cleardevice();
+                    circle(x,++y,50);
+                    delay(3);
+                }
+            }
+            delay(5);
+        }
+    }
+
+};
 
 int main() {
 
+    //Getting the screen dimensions
     DWORD screenWidth = GetSystemMetrics(SM_CXSCREEN);
     DWORD screenHeight = GetSystemMetrics(SM_CYSCREEN);
 
     initwindow(screenWidth,screenHeight,"",-3,-3);
 
-    int page=0, barX=200;
+    int page=0;
 
-    int x=50,y=100;
+
     circle(x,y,50);
 
-    while(1) {
+    Mario mario;
 
-        if(GetAsyncKeyState(VK_RIGHT)) {
+    thread t1(Mario::moveLeft,mario);
+    thread t2(Mario::moveRight,mario);
+    thread t3(Mario::jump,mario);
 
-        cleardevice();circle(++x,y,50);
-        }
-        if(GetAsyncKeyState(VK_LEFT)) {
-
-        cleardevice();
-            circle(--x,y,50);
-        }
-        if(GetAsyncKeyState(VK_SPACE)) {
-
-            for(int i=0; i<50;i++) {
-                    if(GetAsyncKeyState(VK_RIGHT)) {
-
-        cleardevice();circle(++x,y,50);
-        }
-                    cleardevice();
-                    circle(x,--y,50);
-                    delay(5);
-            }
-            for(int i=0; i<50;i++) {
-                    cleardevice();
-                    circle(x,++y,50);
-                    delay(5);
-            }
+    t1.join();
 
 
-        }
-        delay(5);
-    }
+
 }
